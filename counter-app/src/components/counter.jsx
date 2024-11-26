70879e796799a2f0c81167aa602d5896585b740f
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-26 00:43:19 -0800

    Refactor Counter component to include dynamic badge classes for count display

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -7,12 +7,15 @@ class Counter extends Component {
     render() { 
         return (
             <div>
-                <span>{this.formatCount()}</span>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
-
         );
-       
+    }
+    getBadgeClasses(){
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes
     }
     formatCount(){
         const { count } = this.state;
