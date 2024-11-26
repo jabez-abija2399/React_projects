fc0981e063ad13af27770d62a6302458eabeb80b
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-26 07:55:02 -0800

    Reset initial count state to 0 in Counter component

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
   };
   handleIncrement = () => {
     this.setState({ count: this.state.count + 1 });
