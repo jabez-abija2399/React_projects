606a1e3fdaba9eab5797a466df2a9ebcb60162a5
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-26 01:02:17 -0800

    Update Counter component to initialize count at 1 and handle increment action

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,18 +2,22 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = { 
-        count: 0
+        count: 1
      } 
+     handleIncrement(){ 
+            console.log('Increment Clicked', this)
+        }   
+
     render() { 
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
     getBadgeClasses(){
-        let classes = "badge m-2 badge-";
+        let classes = "badge m-2 bg-";
         classes += (this.state.count === 0) ? "warning" : "primary";
         return classes
     }
