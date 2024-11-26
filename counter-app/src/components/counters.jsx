fb7f41769a7d12946adda75414653e08ec3f405b
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-26 09:04:00 -0800

    Add increment and decrement methods to Counters component

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -4,11 +4,27 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 4 },
+      { id: 1, value: 0 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
     ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
   };
 
   handleReset = () => {
@@ -32,6 +48,8 @@ class Counters extends Component {
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
             counter={counter}
           />
         ))}
