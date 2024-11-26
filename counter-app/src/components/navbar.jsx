a9b1e0547dc5eb790f63a7a6797022aacf38fe86
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-26 10:25:19 -0800

    Update Navbar component to display total counters in the badge

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -6,7 +6,11 @@ class Navbar extends Component {
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Navbar
+            Navbar{" "}
+            <span className="badge bg-pill bg-secondary">
+              {" "}
+              {this.props.totalCounters}
+            </span>
           </a>
         </div>
       </nav>
