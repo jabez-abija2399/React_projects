6b945ccbf79a4efbe3f03078d9a497ee778a00d5
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-26 10:30:41 -0800

    Refactor Navbar component to use functional component syntax and destructure props

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,21 +1,16 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar{" "}
-            <span className="badge bg-pill bg-secondary">
-              {" "}
-              {this.props.totalCounters}
-            </span>
-          </a>
-        </div>
-      </nav>
-    );
-  }
+const Navbar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="badge bg-pill bg-secondary"> {totalCounters}</span>
+        </a>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
