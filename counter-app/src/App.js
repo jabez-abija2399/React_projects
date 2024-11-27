ff79c12b0abfbe62f9a94b133f38069d49f80e18
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-27 09:56:05 +0300

    Refactor App component to enhance code readability and maintainability

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,64 +1,64 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import NavBar from "./components/navbar";
-import Counters from "./components/counters";
-import "./App.css";
+import React, { Component } from "react"; // Import React and Component from react library
+import logo from "./logo.svg"; // Import logo image
+import NavBar from "./components/navbar"; // Import NavBar component
+import Counters from "./components/counters"; // Import Counters component
+import "./App.css"; // Import CSS file for styling
 
-class App extends Component {
+class App extends Component { // App component is a class component 
   state = {
     counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
+      { id: 1, value: 0 }, // Counter object with id 1 and initial value 0
+      { id: 2, value: 0 }, // Counter object with id 2 and initial value 0
+      { id: 3, value: 0 }, // Counter object with id 3 and initial value 0
+      { id: 4, value: 0 }, // Counter object with id 4 and initial value 0
     ],
   };
 
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState(() => ({ counters }));
-  };
+  handleIncrement = (counter) => { // Handle incrementing value of counter object in counters array 
+    const counters = [...this.state.counters]; // Clone counters array to avoid direct mutation of state object 
+    const index = counters.indexOf(counter);  // Get index of counter object in counters array   
+    counters[index] = { ...counter }; // Clone counter object to avoid direct mutation of state object 
+    counters[index].value++;  // Increment value of counter object 
+    this.setState(() => ({ counters })); // Update state object with new counters array 
+  }; 
 
-  handleDecrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
+  handleDecrement = (counter) => { // Handle decrementing value of counter object in counters array
+    const counters = [...this.state.counters]; // Clone counters array to avoid direct mutation of state object
+    const index = counters.indexOf(counter);  // Get index of counter object in counters array
 
     if (counters[index].value === 0) return null; // Prevent decrementing below 0 value of counter
 
-    counters[index] = { ...counter };
-    counters[index].value--;
-    this.setState({ counters });
+    counters[index] = { ...counter }; // Clone counter object to avoid direct mutation of state object
+    counters[index].value--; // Decrement value of counter object
+    this.setState({ counters }); // Update state object with new counters array
   };
 
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
+  handleReset = () => { // Handle resetting all counter values to 0
+    const counters = this.state.counters.map((c) => { // Map over counters array
+      c.value = 0; // Set value of each counter to 0
+      return c; // Return updated counter object
     });
-    this.setState({ counters });
+    this.setState({ counters }); // Update state object with new counters array
   };
 
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
+  handleDelete = (counterId) => { // Handle deleting a counter object from counters array
+    const counters = this.state.counters.filter((c) => c.id !== counterId); // Filter out counter object with matching id
+    this.setState({ counters }); // Update state object with new counters array
   };
 
-  render() {
+  render() { // Render method to display components
     return (
-      <React.Fragment>
+      <React.Fragment> {/* React Fragment to group multiple elements */}
         <NavBar
-          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length} // Pass total number of counters with value > 0 to NavBar component
         />
-        <main className="container">
+        <main className="container"> {/* Main container for the app */}
           <Counters
-            counters={this.state.counters}
-            onReset={this.handleReset}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onDelete={this.handleDelete}
+            counters={this.state.counters} // Pass counters array to Counters component
+            onReset={this.handleReset} // Pass handleReset method to Counters component
+            onIncrement={this.handleIncrement} // Pass handleIncrement method to Counters component
+            onDecrement={this.handleDecrement} // Pass handleDecrement method to Counters component
+            onDelete={this.handleDelete} // Pass handleDelete method to Counters component
           />
         </main>
       </React.Fragment>
@@ -66,4 +66,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App; // Export App component as default export
