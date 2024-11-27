8c416ea146f6defd987a92073072d98bfc8a054c
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-27 09:41:56 +0300

    Refactor Counters component to improve button formatting and readability

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -3,15 +3,12 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
-    
-    const { onReset, counters, onDelete, onIncrement, onDecrement } = this.props;
+    const { onReset, counters, onDelete, onIncrement, onDecrement } =
+      this.props;
 
     return (
       <div>
-        <button
-          onClick={onReset}
-          className="btn btn-primary btn-sm m-2"
-        >
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
           {" "}
           Reset
         </button>
