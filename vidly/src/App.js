0b76dbbd3ac9f86e0cb35e5ce0aefcfe10d8231d
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-28 10:32:49 +0300

    Add Font Awesome CSS for enhanced icon support in App component

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
 import "bootstrap/dist/css/bootstrap.min.css"; 
+import "font-awesome/css/font-awesome.css";
 import "./App.css";
 
 function App() {
