34a11fadc8fe69da5040f1919c51623c118a83ef
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-28 18:36:16 +0300

    Remove console logs from paginate utility for cleaner code

--- vidly/src/utils/paginate.js
+++ vidly/src/utils/paginate.js
@@ -2,11 +2,7 @@ import _ from "lodash";
 
 export function paginate(items, pageNumber, pageSize) {
   // function to paginate an array of items
-  console.log("items", items);
-  console.log("pageNumber", pageNumber);
-  console.log("pageSize", pageSize);
   const startIndex = (pageNumber - 1) * pageSize; // calculate the start index of the items on the current page
-  console.log("startIndex", startIndex);
   return _(items) // convert the items array to a lodash wrapper
     .slice(startIndex) // slice the array starting from the start index
     .take(pageSize) // take the number of items equal to the pageSize
