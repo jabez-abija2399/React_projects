b007b866570714af520d346b585561bc73a10261
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-28 18:45:13 +0300

    Add PropTypes validation to Pagination component props

--- vidly/src/components/comman/pagination.jsx
+++ vidly/src/components/comman/pagination.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import _ from 'lodash';
+import PropTypes from 'prop-types';
 
 const Pagination = props => {
     const { itemsCount, pageSize, currentPage, onPageChange } = props;  // object destructuring to extract the itemsCount, pageSize, currentPage, and onPageChange properties from props
@@ -19,6 +20,13 @@ const Pagination = props => {
         </nav>
         
      );
+}
+
+Pagination.propTypes = {
+    itemsCount: PropTypes.number.isRequired,  // itemsCount prop is required and must be a number
+    pageSize: PropTypes.number.isRequired,  // pageSize prop is required and must be a number
+    currentPage: PropTypes.number.isRequired,  // currentPage prop is required and must be a number
+    onPageChange: PropTypes.func.isRequired  // onPageChange prop is required and must be a function
 }
  
 export default Pagination;
