087504554b5db6128d7e12dbfb6df14457636bb7
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-28 18:24:40 +0300

    Remove console logs from Pagination component and add debug logs in paginate utility

--- vidly/src/components/comman/pagination.jsx
+++ vidly/src/components/comman/pagination.jsx
@@ -3,7 +3,6 @@ import _ from 'lodash';
 
 const Pagination = props => {
     const { itemsCount, pageSize, currentPage, onPageChange } = props;  // object destructuring to extract the itemsCount, pageSize, currentPage, and onPageChange properties from props
-    console.log(currentPage);
     const pagesCount = Math.ceil(itemsCount / pageSize);  // calculate the number of pages by dividing the itemsCount by the pageSize and rounding up to the nearest whole number
     if (pagesCount === 1) return null;  // if there is only one page, return null   
     const pages = _.range(1, pagesCount + 1);  // create an array of page numbers from 1 to pagesCount
