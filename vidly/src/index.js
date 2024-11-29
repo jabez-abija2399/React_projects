00c77b83a861a1eac682460bb25dc42aa9bcea4e
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-29 18:24:42 +0300

    Add Rental component and update index.js for routing

--- vidly/src/index.js
+++ vidly/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {}
+import {BrowserRouter } form 'react-dom-route'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
