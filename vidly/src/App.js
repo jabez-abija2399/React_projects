3ecddf6f7dc42cbca749654b22c8047a7120ed05
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-29 19:07:02 +0300

    Refactor App component to implement routing with React Router and add new components

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
-import "bootstrap/dist/css/bootstrap.min.css"; 
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
+import { Route, Navigate, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.css";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Movies />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Routes>
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/rentals" element={<Rentals />} />
+            <Route path="/movies/:id" element={<MovieForm />} />
+            <Route path="/not-found" element={<NotFound />} />
+            <Route path="/" element={<Navigate to="/movies" />} />
+          </Routes>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
