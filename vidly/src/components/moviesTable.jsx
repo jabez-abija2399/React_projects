9cef218bc1f4936f827999e7ecede350b0640646
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-29 12:35:10 +0300

    Add Table component to encapsulate TableHeader and TableBody functionality in MoviesTable

--- vidly/src/components/moviesTable.jsx
+++ vidly/src/components/moviesTable.jsx
@@ -1,62 +1,42 @@
-import React, {Component} from "react";
-import TableHeader from "./comman/tableHeader";
+import React, { Component } from "react";
 import Like from "./comman/like";
-import TableBody from "./comman/tableBody";
+import Table from "./comman/table";
 
 class MoviesTable extends Component {
-    columns = [ 
-        {path: "title", label: "Title"},
-        {path: "genre.name", label: "Genre"},
-        {path: "numberInStock", label: "Stock"},
-        {path: "dailyRentalRate", label: "Rate"},
-        {key: "like" , content: movie =>( <Like
-            liked={movie.liked}
-            onClick={() => this.props.onLike(movie)}
-        />)},
-        {key: "delete", content: movie => (<button onClick={() => this.props.onDelete(movie)} className="btn btn-danger btn-sm">Delete</button>)}
-    ];
-    render() { 
-        const { movies, onDelete, onLike,onSort, sortColumn } = this.props; // object destructuring to extract the movies, onDelete, and onLike properties from props
-  return (
-    <table className="table">
-        <TableHeader
-            columns={this.columns}
-            sortColumn={sortColumn}
-            onSort={onSort}  
-        />
-        <TableBody 
-            data={movies}
-            columns={this.columns}
-        />
-      {/* <tbody>
-        
-        {movies.map((movie) => (
-          <tr key={movie._id}>
-            <td>{movie.title}</td>
-            <td>{movie.genre.name}</td>
-            <td>{movie.numberInStock}</td>
-            <td>{movie.dailyRentalRate}</td>
-            
-            <td>
-                <Like
-                    liked={movie.liked}
-                    onClick={() => onLike(movie)}
-                />
-            </td>
-            <td>
-              <button
-                onClick={() => onDelete(movie)}
-                className="btn btn-danger btn-sm"
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody> */}
-    </table>
-  );
-};
+  columns = [
+    { path: "title", label: "Title" },
+    { path: "genre.name", label: "Genre" },
+    { path: "numberInStock", label: "Stock" },
+    { path: "dailyRentalRate", label: "Rate" },
+    {
+      key: "like",
+      content: (movie) => (
+        <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
+      ),
+    },
+    {
+      key: "delete",
+      content: (movie) => (
+        <button
+          onClick={() => this.props.onDelete(movie)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      ),
+    },
+  ];
+  render() {
+    const { movies, onSort, sortColumn } = this.props; // object destructuring to extract the movies, onDelete, and onLike properties from props
+    return (
+      <Table // Table component
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
+    );
+  }
 }
 
 export default MoviesTable; // export the MoviesTable component
