92097170a0233c3304a6ba6f9d1e00b125fd9e2c
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-29 20:50:18 +0300

    Refactor routing in App component to include BrowserRouter and streamline route definitions

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -5,7 +5,7 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
-import { Route, Navigate, Routes } from "react-router-dom";
+import { Route, Navigate, Routes, BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.css";
 import "./App.css";
@@ -13,14 +13,17 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
+
       <React.Fragment>
+
         <NavBar />
         <main className="container">
+          
           <Routes>
+              <Route path="/movie/:id" element={<MovieForm />} />
             <Route path="/movies" element={<Movies />} />
             <Route path="/customers" element={<Customers />} />
             <Route path="/rentals" element={<Rentals />} />
-            <Route path="/movies/:id" element={<MovieForm />} />
             <Route path="/not-found" element={<NotFound />} />
             <Route path="/" element={<Navigate to="/movies" />} />
           </Routes>
