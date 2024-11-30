8686721786c911aac4f5cabc902b0013b323e5ef
jabez-abija2399 <jabeznew2399@gmail.com>
2024-11-30 22:31:42 +0300

    Refactor LoginForm component to handle form submission and improve structure

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,26 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class LoginForm extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <div>
-                <h1>Login</h1>
-                <form>
-                    <div className='form-group'>
-                        <label htmlFor='username'>Username</label>
-                        <input id='username' type='text' className='form-control' />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor='password'>Password</label>
-                        <input id='password' type='text' className='form-control' />
-                    </div>
-                    <button className='btn btn-primary'>Login</button>
-                    
-                </form>
-            </div>
-        );
-    }
+  state = {};
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Call the server
+    console.log("Submitted");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="text" className="form-control" />
+          </div>
+          <button className="btn btn-primary m-2">Login</button>
+        </form>
+      </div>
+    );
+  }
 }
- 
+
 export default LoginForm;
