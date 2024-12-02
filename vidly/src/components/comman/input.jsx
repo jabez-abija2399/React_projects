8b3b8cd1b7e540868cd3fb46443ef70202627cf2
jabez-abija2399 <jabeznew2399@gmail.com>
2024-12-02 11:10:49 +0300

    Add error display to Input component for improved user feedback

--- vidly/src/components/comman/input.jsx
+++ vidly/src/components/comman/input.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const Input = ({ name, label, value, onChange }) => {
-
-    return ( 
-        <div className="mb-3">
-            <label htmlFor={name} className="form-label">{label}</label>
-            <input 
-                value={value}
-                onChange={onChange}
-                name={name}
-                id={name}
-                type="text"
-                className="form-control"
-            />
-        </div>
-     );
-}
+const Input = ({ name, label, value, onChange, error }) => {
+  return (
+    <div className="mb-3">
+      <label htmlFor={name} className="form-label">
+        {label}
+      </label>
+      <input
+        value={value}
+        onChange={onChange}
+        name={name}
+        id={name}
+        type="text"
+        className="form-control"
+      />
+      {error && <div className="alert alert-danger mt-2">{error}</div>}
+    </div>
+  );
+};
 
 export default Input;
