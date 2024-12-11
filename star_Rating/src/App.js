c1ea13c47f07ad3b9d66e63140c02174aa520cfd
jabez-abija2399 <jabeznew2399@gmail.com>
2024-12-12 01:30:55 +0300

    Replace default content in App component with StarRating component

--- star_Rating/src/App.js
+++ star_Rating/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import StarRating from './components';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <StarRating />
     </div>
   );
 }
