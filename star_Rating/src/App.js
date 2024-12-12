1243ed70a2dcb3fba3753b5b019b23241476ed00
jabez-abija2399 <jabeznew2399@gmail.com>
2024-12-12 16:46:12 +0300

    Add ImageSlider component and integrate it into App

--- star_Rating/src/App.js
+++ star_Rating/src/App.js
@@ -1,10 +1,15 @@
 import StarRating from './components';
+import ImageSlider from './components/imageSlider';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <StarRating />
+      {/* <StarRating /> */}
+      
+      <ImageSlider url={'https://picsum.photos/v2/list'} limit={5} page={1} />
+
     </div>
   );
 }
